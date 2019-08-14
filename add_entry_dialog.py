6604875d37095e52d33840312266fa2b77a05f7e
c-lang18 <carolinalang96@gmail.com>
2019-08-14 17:07:12 +1000

    Updated Popup Dialog
    - Changed dialog layout to vertical

--- add_entry_dialog.py
+++ add_entry_dialog.py
@@ -7,46 +7,38 @@ class addDialog:
         top = self.top = Toplevel(parent)
         top.resizable(False, False)
 
-        label_frame = Frame(top)
-        label_frame.pack(side=TOP, fill=BOTH, expand=FALSE)
-
-        field_frame = Frame(top)
-        field_frame.pack(fill=BOTH)
-
+        vertical_frame = Frame(top)
+        vertical_frame.pack(side=TOP)
+        label_frame = Frame(vertical_frame)
+        label_frame.pack(side=LEFT, fill=BOTH, expand=FALSE)
+        field_frame = Frame(vertical_frame)
+        field_frame.pack(side=RIGHT, fill=BOTH, expand=FALSE)
         button_frame=Frame(top)
-        button_frame.pack(side=BOTTOM)
+        button_frame.pack(side=BOTTOM, fill=BOTH)
 
-
+        # labels
         site_label = Label(label_frame, text="Enter website")
-        site_label.pack(side=LEFT, pady=10, padx=60)
-
+        site_label.pack(pady=10, padx=10)
         email_label = Label(label_frame, text="Enter email")
-        email_label.pack(side=LEFT, pady=10, padx=50)
-
+        email_label.pack(pady=10, padx=10)
         user_label = Label(label_frame, text="Enter username")
-        user_label.pack(side=LEFT, pady=10, padx=60)
-
+        user_label.pack(pady=10, padx=10)
         pass_label = Label(label_frame, text="Enter password")
-        pass_label.pack(side=RIGHT, pady=10, padx=50)
+        pass_label.pack(pady=10, padx=10)
 
-        
-
+        # entries
         self.site = Entry(field_frame, width=30)
-        self.site.pack(side=LEFT, pady=10, padx=5)
-
+        self.site.pack(pady=10, padx=10)
         self.email = Entry(field_frame, width=30)
-        self.email.pack(side=LEFT, pady=10, padx=5)
-
+        self.email.pack(pady=10, padx=10)
         self.user = Entry(field_frame, width=30)
-        self.user.pack(side=RIGHT, pady=10, padx=5)
-
+        self.user.pack(pady=10, padx=10)
         self.password = Entry(field_frame, width=30)
-        self.password.pack(side=RIGHT, pady=10, padx=5)
-    
+        self.password.pack(pady=10, padx=10)
 
-        self.enterDetails_btn = Button(button_frame, text="Add new entry", width=15, command=self.addEntry).pack(side=LEFT, pady=10, padx=40)    
-        self.cancel_btn = Button(button_frame, text="Cancel", width=15).pack(side=RIGHT, pady=10, padx=40)
-
+        #buttons
+        self.enterDetails_btn = Button(button_frame, text="Add new entry", width=15, command=self.addEntry).pack(side=LEFT, pady=10, padx=20)    
+        self.cancel_btn = Button(button_frame, text="Cancel", width=15, command=self.exit).pack(side=RIGHT, pady=10, padx=20)
 
     def addEntry(self):
         entered_site = str(self.site.get())
@@ -56,6 +48,9 @@ class addDialog:
 
         print(entered_site, entered_email, entered_user, entered_password)
 
+        
+    def exit(self):
+        add_dialog_popup.destroy()
 
 #TODO 
 # close popup dialog on button click
